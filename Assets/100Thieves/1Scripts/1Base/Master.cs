01ffbbcd1083714c7a5b45ae798f79da27d63c47
Renat <painren@mail.ru>
2024-10-31 01:24:31 +0300

    add mainMenu screen, button for switch

--- Assets/100Thieves/1Scripts/1Base/Master.cs
+++ Assets/100Thieves/1Scripts/1Base/Master.cs
@@ -5,11 +5,16 @@ using UnityEngine.UI;
 
 public class Master : MonoBehaviour
 {
+    public static Master Instance;
+    public ManagerUI ManagerUI => _managerUI;
+
     [SerializeField] private ManagerUI _managerUI;
 
     void Start()
     {
         DontDestroyOnLoad(this);
+
+        Instance = this;
 
         _managerUI.Setup();
     }
